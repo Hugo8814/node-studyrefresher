5b876881691eae9ac95d4cef601a6ef84250bf1c
Hugo Pereira <128017769+Hugo8814@users.noreply.github.com>
2025-02-13 22:46:08 +0000

    node study

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const fs = require('fs');
+
+const app = express();
+app.use(express.json());
+
+// app.get("/", (req, res)=>{
+// res.status(200).send("Hello from the server");
+// })
+
+// app.post("/", (req, res)=>{
+//     res.status(200).send("post the server");
+// })
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours: tours,
+    },
+  });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  console.log(req.body);
+  res.send("done")
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}...`);
+});
