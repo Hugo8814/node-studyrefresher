7b460dbd0242581a7c0fb0ef93c693677c95f735
Hugo Pereira <128017769+Hugo8814@users.noreply.github.com>
2025-02-20 23:00:40 +0000

    update server.cjs

--- server.cjs
+++ server.cjs
@@ -10,6 +10,23 @@ mongoose
   .then(() => console.log("MongoDB connected successfully!"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
+const tourSchema = new mongoose.Scheme({
+  name: {
+    type: String,
+    required: [true, "A tour must have a name"],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, "A tour must have a price"],
+  },
+});
+
+const Tour = mongoose.model("Tour", tourSchema);
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server running on port ${port}...`);
