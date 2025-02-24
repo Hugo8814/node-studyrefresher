0309c68503276efbecadc61e0081ef3fb300cee4
Hugo Pereira <128017769+Hugo8814@users.noreply.github.com>
2025-02-24 19:58:43 +0000

    some node.js testing

--- controllers/tourController.cjs
+++ controllers/tourController.cjs
@@ -22,7 +22,7 @@ exports.getAllTours = async (req, res) => {
 exports.getTour = async (req, res) => {
   try {
     const tours = await Tour.findById(req.params.id);
-    // const tour = await Tour.findOne({ _id: req.params.id });
+    //const tour = await Tour.findOne({ _id: req.params.id })
 
     res.status(200).json({
       status: "success",
