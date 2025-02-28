260e7734fbea2c506f1cdc942353d2775facc951
Hugo Pereira <128017769+Hugo8814@users.noreply.github.com>
2025-02-28 00:39:59 +0000

    some node.js testing

--- models/tourModel.cjs
+++ models/tourModel.cjs
@@ -48,11 +48,7 @@ const tourSchema = new mongoose.Schema({
     required: [true, "A tour must have a cover image"],
   },
   images: [String],
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-    select: false,
-  },
+ 
   startDates: [Date],
 
 });
