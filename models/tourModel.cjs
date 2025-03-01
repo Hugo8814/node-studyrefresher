60afd5472ea80964eba4ae700afe9e559c021384
Hugo Pereira <128017769+Hugo8814@users.noreply.github.com>
2025-03-01 00:34:44 +0000

    update tourModel.cjs

--- models/tourModel.cjs
+++ models/tourModel.cjs
@@ -5,6 +5,7 @@ const tourSchema = new mongoose.Schema({
     type: String,
     required: [true, "A tour must have a name"],
     unique: true,
+    trim: true,
   },
   maxGroupSize: {
     type: Number,
@@ -31,9 +32,29 @@ const tourSchema = new mongoose.Schema({
     required: [true, "A tour must have a price"],
   },
   priceDiscount: {
-    type: Number,
-     
+    summary:{
+      type: String,
+      trim: true,
+      required: [true, "A tour must have a description"],
+    }
   },
+  description: {
+    type: String,
+    trim: true,
+  },
+  
+  imageCover: {
+    type: String,
+    required: [true, "A tour must have a cover image"],
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+    select: false,
+  },
+  startDates: [Date],
+
 });
 
 const Tour = mongoose.model("tour", tourSchema);
